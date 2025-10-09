f2e58d7725e90c1cf451ad8ab83d5de8b6904960
storm-0rigin <storm85851@ajou.ac.kr>
2025-10-09 10:32:20 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,18 +9,25 @@ contract MyToken{
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals){
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount){
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
         // transaction
         // from, to , data, values, gas ....
-        _mint(1*10*uint256(decimals), msg.sender); // 1MT
+        _mint(_amount*10**uint256(decimals), msg.sender); // 1MT
     }
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] +=  amount;
     }
+
+    function transfer(uint256 amount, address to) external{
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
+
 
 }
