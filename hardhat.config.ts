2fc1ec747a753f97280ba8d637eb2f3ac9398021
storm-0rigin <storm85851@ajou.ac.kr>
2025-11-26 19:22:38 +0900

    official last commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,21 @@ const config: HardhatUserConfig = {
       ],
     },
   },
+  etherscan: {
+    apiKey: {
+      kairos: "unnecessary",
+    },
+    customChains: [
+      {
+        network: "kairos",
+        chainId: 1001,
+        urls: {
+          apiURL: "https://kairos-api.kaiascan.io/hardhat-verify",
+          browserURL: "https://kairos.kaiascan.io",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
